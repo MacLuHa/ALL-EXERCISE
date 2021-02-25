3e88ebe02b2b153ce17ef3e32f3ac7cd5f36c8a6
Firestorm <schuhtuev.maxim@yandex.ru>
2021-02-25 18:44:29 +0300

    Добавил решение задач из 21-ой главы

---
+++ Allexer/Глава 21/Глава 21(номер 1).py
@@ -0,0 +1,23 @@
+class Stack():
+    def __init__(self):
+        self.items=[]
+    def is_empty(self):
+        return self.items==[]
+    def push(self,item):
+        self.items.append(item)
+    def pop(self):
+        return self.items.pop()
+    def peek(self):
+        last=len(self.items)-1
+        return self.items[last]
+    def size(self):
+        return len(self.items)
+
+stack=Stack()
+for i in range(1,6):
+    stack.push(i)
+reverse_stack=[]
+for i in range(len(stack.items)):
+    reverse_stack.append(stack.pop())
+print(reverse_stack)
+
